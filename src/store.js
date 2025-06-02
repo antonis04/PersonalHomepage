78bcce8e5d64498d717049e7220f0ca82ef0e5ea
Antoni Samol <antoni.samol@gmail.com>
2025-06-02 17:19:59 +0200

    local store created

---
+++ src/store.js
@@ -0,0 +1,18 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { myHomepageReducer } from "../features/Homepage/homepageSlice";
+import rootSaga from "./rootSaga";
+import createSagaMiddleware from "redux-saga";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
+    reducer: {
+        homepage: myHomepageReducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(sagaMiddleware),
+});
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
