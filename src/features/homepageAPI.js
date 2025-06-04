2ac89b4f3b43e5844cc5fa84ab1b23e314d17063
Antoni Samol <antoni.samol@gmail.com>
2025-06-04 21:23:52 +0200

    api moved to new folder

---
+++ src/features/homepageAPI.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const GITHUB_API_BASE_URL = "https://api.github.com/users";
+
+/**
+ * Fetches user repositories from GitHub API
+ * @param {string} githubUsername - The GitHub username
+ * @returns {Promise} Response data containing repositories
+ */
+export const getUserRepositories = async (githubUsername) => {
+  const apiResponse = await axios.get(
+    `${GITHUB_API_BASE_URL}/${githubUsername}/repos`
+  );
+  return apiResponse.data;
+};
