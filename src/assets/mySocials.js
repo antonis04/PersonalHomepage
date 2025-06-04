6e79e82b768789991180d394adedace45d6bece7
Antoni Samol <antoni.samol@gmail.com>
2025-06-04 10:54:47 +0200

    footer roganized

--- src/assets/mySocials.js
+++ src/assets/mySocials.js
@@ -4,13 +4,13 @@ import { StyleIcon } from "../atributes/homepage/Footer/Socials/styled";
 
 export const socials = [
   {
-    name: "LinkedIn",
-    url: "https://www.linkedin.com/in/antoni-samol-44649a368/",
-    Icon: StyleIcon(LinkedIn),
-  },
-  {
     name: "GitHub",
     url: "https://github.com/antonis04",
     Icon: StyleIcon(GitHub),
+  },
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/antoni-samol-44649a368/",
+    Icon: StyleIcon(LinkedIn),
   },
 ];
