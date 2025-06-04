2560e9ba16f4665dee6adef5be91bc1596137245
Antoni Samol <antoni.samol@gmail.com>
2025-06-04 21:23:38 +0200

    comment + margin changed

--- src/common/Header.js
+++ src/common/Header.js
@@ -1,15 +1,16 @@
 import styled from "styled-components";
 
+// Main title/name header with responsive typography
 export const NameHeader = styled.h1`
-    font-size: 38px;
-    font-weight: 900;
-    margin: 0 0 35px 0;
-    padding: 0;
-    line-height: 1.0;
-    color: ${({ theme }) => theme.colors.header.headerName};
+  font-size: 38px;
+  font-weight: 900;
+  margin: 0 0 35px 0;
+  padding: 0;
+  line-height: 1;
+  color: ${({ theme }) => theme.colors.header.headerName};
 
-    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
-        font-size: 22px;
-        margin: 0 0 16px 0;
-    }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    font-size: 22px;
+    margin: 0 0 16px 0;
+  }
 `;
