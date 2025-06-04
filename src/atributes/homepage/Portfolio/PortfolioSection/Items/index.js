dae704b36b8bbc38da5ea194dbd6658f2c95b60a
Antoni Samol <antoni.samol@gmail.com>
2025-06-04 11:51:41 +0200

    gh pages set up

--- src/atributes/homepage/Portfolio/PortfolioSection/Items/index.js
+++ src/atributes/homepage/Portfolio/PortfolioSection/Items/index.js
@@ -3,18 +3,27 @@ import { Description, ProjectName } from "./styled";
 import { LinkContainer } from "../Links";
 
 export const ProjectItem = ({ project }) => {
-  const { name, description, homepage, html_url } = project;
+  const { name, description, homepage, html_url, owner, fork, private: isPrivate } = project;
+
+  // Generate demo URL: use homepage if set, otherwise generate GitHub Pages URL for public repos
+  let demoUrl = homepage;
+  if (!demoUrl && !fork && !isPrivate && owner && owner.login) {
+    // Generate GitHub Pages URL for all public, non-fork repos
+    demoUrl = `https://${owner.login}.github.io/${name}/`;
+  }
 
   return (
     <Project>
       <ProjectName>{name}</ProjectName>
       <Description>{description}</Description>
+      {demoUrl && (
+        <LinkContainer 
+          description={"Demo:"} 
+          href={demoUrl} />
+      )}
       <LinkContainer 
-        description={"Demo:"} 
-        href={homepage} />
-      <LinkContainer 
-      description={"Code:"} 
-      href={html_url} />
+        description={"Code:"} 
+        href={html_url} />
     </Project>
   );
 };
