9be600b2d7f6dd75584f376c1734eff772228570
Antoni Samol <antoni.samol@gmail.com>
2025-06-04 21:19:22 +0200

    class name changed

--- src/assets/mySocials.js
+++ src/assets/mySocials.js
@@ -1,16 +1,16 @@
 import { ReactComponent as LinkedIn } from "./svg/LinkedIN.svg";
 import { ReactComponent as GitHub } from "./svg/Github.svg";
-import { StyleIcon } from "../atributes/homepage/Footer/Socials/styled";
+import { StyledSocialIcon } from "../features/mainPage/Footer/Socials/styled";
 
 export const socials = [
   {
     name: "GitHub",
     url: "https://github.com/antonis04",
-    Icon: StyleIcon(GitHub),
+    Icon: StyledSocialIcon(GitHub),
   },
   {
     name: "LinkedIn",
     url: "https://www.linkedin.com/in/antoni-samol-44649a368/",
-    Icon: StyleIcon(LinkedIn),
+    Icon: StyledSocialIcon(LinkedIn),
   },
 ];
